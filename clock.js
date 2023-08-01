3b332be3cb37cfdaf11353aaf461e9f48410a062
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-08-01 15:28:11 +0530

    added seconds back

--- clock.js
+++ clock.js
@@ -4,7 +4,7 @@ let clock = document.getElementById('clock');
 function updateClock() {
     new Date().toLocaleTimeString();
     //format the time to 12 hour format and no am,pm
-    let time = new Date().toLocaleTimeString('en-US', { hour12: true, hour: 'numeric', minute: 'numeric'});
+    let time = new Date().toLocaleTimeString('en-US', { hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
     clock.innerHTML = time;
 }
 
