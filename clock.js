ced3e9178e7ab6102d863d235046a71804afea78
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-08-01 15:24:08 +0530

    initial commit

---
+++ clock.js
@@ -0,0 +1,11 @@
+//a script that will update time in the clock div realtime
+let clock = document.getElementById('clock');
+
+function updateClock() {
+    new Date().toLocaleTimeString();
+    //format the time to 12 hour format and no am,pm
+    let time = new Date().toLocaleTimeString('en-US', { hour12: true, hour: 'numeric', minute: 'numeric'});
+    clock.innerHTML = time;
+}
+
+setInterval(updateClock, 1000);
